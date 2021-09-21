3915871331513e455fb2a090c8165e129e4b0d27
Wei Tang <tangwei@smail.nju.edu.cn>
2021-09-21 20:12:59 +0800

    rm deps

--- deps/build.jl
+++
@@ -1,9 +0,0 @@
-# see https://gist.github.com/Luthaf/368a23981c8ec095c3eb
-import PyCall: pyimport
-
-# See https://stackoverflow.com/questions/12332975/installing-python-module-within-code.
-const PIP_PACKAGES = ["numpy", "py3nj"]
-
-sys = pyimport("sys")
-subprocess = pyimport("subprocess")
-subprocess.check_call([sys.executable, "-m", "pip", "install", "--user", "--upgrade", PIP_PACKAGES...])
